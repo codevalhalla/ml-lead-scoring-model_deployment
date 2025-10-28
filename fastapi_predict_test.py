5ee7f3b4e7a56f0df3186d1d73c43cd4847f6a9e
codevalhalla <prsk3496@gmail.com>
2025-10-28 18:59:52 +0530

    Update client data in prediction test script for lead scoring model

--- fastapi_predict_test.py
+++ fastapi_predict_test.py
@@ -5,10 +5,10 @@ import requests
 
 url = "http://0.0.0.0:9696/predict"
 
-client = {
-    "lead_source": "organic_search",
-    "number_of_courses_viewed": 4,
-    "annual_income": 80304.0
+client={
+    "lead_source": "paid_ads",
+    "number_of_courses_viewed": 2,
+    "annual_income": 79276.0
 }
 
 response = requests.post(url, json=client)
