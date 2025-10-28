93f0609b85136c028d2df1a8d8b6dc69e5f4327d
codevalhalla <prsk3496@gmail.com>
2025-10-28 16:46:56 +0530

    Add initial FastAPI prediction test script for lead scoring model

---
+++ fastapi_predict_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+
+url = "http://127.0.0.1:8000/predict"
+
+client = {
+    "lead_source": "organic_search",
+    "number_of_courses_viewed": 4,
+    "annual_income": 80304.0
+}
+
+response = requests.post(url, json=client)
+
+if response.status_code == 200:
+    print("Lead Conversion Probability:", response.json()["lead_conversion_probability"])
+else:
+    print("Error:", response.status_code, response.text)
