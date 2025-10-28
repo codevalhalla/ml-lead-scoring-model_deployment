54eeca6bcc46898e303c1d8677f253ca7d8d2cd3
codevalhalla <prsk3496@gmail.com>
2025-10-28 16:17:29 +0530

    Initial commit of FastAPI lead scoring application with prediction endpoint and model loading functionality

---
+++ fastapi_python_predict.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pickle
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+
+input_file = "pipeline_v1.bin"
+
+with open(input_file,'rb') as f_in:
+    dv,model = pickle.load(f_in)
+    
+app = FastAPI('fastapi_lead_scoring_app')
+
+class LeadDetails(BaseModel):
+    lead_source: str
+    number_of_courses_viewed: int
+    annual_income: float
+    
+
+@app.post("/predict")
+def predict(lead_data: LeadDetails):
+    lead_dict = lead_data.dict()
+    X = dv.transform([lead_dict])
+    y_pred = model.predict_proba(X)[0,1]
+    
+    result = {
+        'lead_conversion_probability': float(y_pred)
+    }
+    
+    return result
